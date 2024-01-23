649c327c03bd0bf07fbb3df524ef52d0e77ff8d1
Amaia <54155@novasbe.pt>
2024-01-23 17:10:37 +0100

    directory organization

---
+++ part1/counter/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App counter={counter} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
